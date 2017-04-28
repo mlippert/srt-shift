2b73a1dccd128b410cfcc5b795714aa5b6faed88
Mike Lippert <mlippert255@gmail.com>
2017-04-27 21:53:25 -0400

    main loop basics to get subtitle, shift time, write subtitle w/ no actual file reading yet

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { TimeSpan } from 'timespan';
+import { TimeSpan, clone as cloneTS } from 'timespan';
 import parseArgs = require('minimist');
 import Subtitle from './subtitle';
 
@@ -14,3 +14,41 @@ console.dir(s1);
 
 s1.adjustTime(new TimeSpan(-10, -1));
 console.dir(s1);
+
+
+// Open the given srt file
+
+// Create a read stream
+
+function getSubtitle(verifySubtitleNum: number): Subtitle | null
+{
+    if (verifySubtitleNum > 4)
+        return null;
+
+    let startTime = new TimeSpan(verifySubtitleNum * 1000);
+    let endTime = cloneTS(startTime);
+    endTime.addMilliseconds(1000);
+    return new Subtitle({ startTime, endTime, text: [`${verifySubtitleNum}`] });
+}
+
+// Create a write stream
+
+function writeSubtitle(st: Subtitle): void
+{
+    console.dir(st);
+}
+
+// Loop reading 1 subtitle at a time shifting its display time and writing it to the
+// output file.
+
+
+// TODO: get timeshift from commandline
+let timeshift = new TimeSpan(-450);
+
+for (let subtitleNum = 1, st = getSubtitle(subtitleNum);
+     st !== null;
+     st = getSubtitle(++subtitleNum))
+{
+    st.adjustTime(timeshift);
+    writeSubtitle(st);
+}
