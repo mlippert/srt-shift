66de39b06d002df1094da1b8f387d91c7277bfac
Mike Lippert <mlippert255@gmail.com>
2017-05-18 16:02:02 -0400

    Add Subtitle method toSrt(). Also work on jsdoc comments removing redundant type info (specified in typescript sytax).

--- src/subtitle.ts
+++ src/subtitle.ts
@@ -24,7 +24,11 @@ import { TimeSpan } from 'timespan';
 /* ******************************************************************************
  * Subtitle                                                                */ /**
  *
- * [Description of the Subtitle class]
+ * A subtitle is text transcribing what is being said in a video at a particular
+ * time which is displayed on top of that video for a limited duration.
+ * As such it contains the text to be displayed (multiline, possibly w/ embedded
+ * formatting) the time offset from the start of the video when the text should
+ * be displayed, and the time offset when that text should no longer be displayed.
  *
  ********************************************************************************/
 export default class Subtitle
@@ -42,14 +46,10 @@ export default class Subtitle
      * constructor                                                         */ /**
      *
      * Subtitle class constructor.
-     *
-     * @param {!Subtitle.Config=} config
-     *      The settings to configure this Subtitle
-     *
      */
     constructor({ startTime = new TimeSpan(),
                   endTime = new TimeSpan(),
-                  text = [] }: { startTime?: TimeSpan, endTime?: TimeSpan, text?: string[] } = {})
+                  text = [] }: Subtitle_Config = {})
     {
         // instance properties
         this.startTime = startTime;
@@ -63,7 +63,7 @@ export default class Subtitle
      * Adjust the time that the subtitle is displayed by the given timespan. To
      * display earlier, the given timespan should be negative.
      *
-     * @param {TimeSpan} offset
+     * @param offset
      *      Amount of time to adjust when the subtitle is displayed.
      */
     adjustTime(offset: TimeSpan): void
@@ -71,17 +71,72 @@ export default class Subtitle
         this.startTime.add(offset);
         this.endTime.add(offset);
     }
+
+    /* **************************************************************************
+     * toSrt                                                               */ /**
+     *
+     * Get the subtitle as an array of lines in the SRT format. The lines
+     * will not include an initial line w/ the subtitle index number as it is
+     * not known, nor will it include the trailing empty line signifying the
+     * end of the subtitle text.
+     */
+    toSrt(): string[]
+    {
+        let srtLines: string[] =
+            [
+                `${toSrtTimeString(this.startTime)} --> ${toSrtTimeString(this.endTime)}`, // 00:00:05,806 --> 00:00:06,905
+                ...this.text
+            ];
+
+        return srtLines;
+    }
 }
 
 /* ******************************************************************************
- * Subtitle.Config                                                         */ /**
+ * Subtitle_Config                                                         */ /**
  *
- * The Subtitle.Config defines the object passed to the constructor that
+ * The Subtitle_Config defines the object passed to the constructor that
  * contains options/values used to initialize an instance of a Subtitle.
  *
- * @typedef {!Object} Subtitle.Config
+ * @property startTime
+ *      The time offset into the video when the subtitle should start being
+ *      overlayed onto the video. Defaults to 00:00:00,000
  *
- * @property {string | undefined} property1_name
- *      [description of property1_name]
+ * @property endTime
+ *      The time offset into the video when the overlayed subtitle should be
+ *      removed from the video, must be greater than or equal to the startTime.
+ *      Defaults to 00:00:00,000
+ *
+ * @property text
+ *      The array of text lines that are overlayed on the video. Defaults to
+ *      an empty array (no text).
  */
+interface Subtitle_Config
+{
+    startTime?: TimeSpan;
+    endTime?: TimeSpan;
+    text?: string[];
+}
 
+
+/* ******************************************************************************
+ * toSrtTimeString                                                         */ /**
+ *
+ * Get the SRT format representation of a given TimeSpan.
+ *
+ * @param t
+ *      The TimeSpan whose SRT representation is desired
+ *
+ * @returns SRT formatted offset time:
+ *      * hh:mm:ss,fff where each value is zero left padded.
+ *          * hh - hours; mm - minutes; ss - seconds; fff - milliseconds
+ */
+function toSrtTimeString(t: TimeSpan): string
+{
+    let h = (t.hours < 10 ? "0" : "") + t.hours.toFixed();
+    let m = (t.minutes < 10 ? "0" : "") + t.minutes.toFixed();
+    let s = (t.seconds < 10 ? "0" : "") + t.seconds.toFixed();
+    let ms = (t.milliseconds < 100 ? "0" : "") + (t.milliseconds < 10 ? "0" : "") + t.milliseconds.toFixed();
+
+    return `${h}:${m}:${s},${ms}`;
+}
